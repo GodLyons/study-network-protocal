ec8f113a4b0ed38c98d97742a4e9ad7a8d3fd592
Lyons <qdgodlyons@163.com>
2021-05-25 22:55:52 +0800

    feat(server):给服务器添加解析body的中间件

--- create-server.js
+++ create-server.js
@@ -1,8 +1,10 @@
 const Koa = require('koa')
 const cors = require('@koa/cors')
+const bodyParser = require('koa-bodyparser')
 
 const app = new Koa()
 app.use(cors())
+   .use(bodyParser())
 app.listen(3000, () => {
   console.log('server is running...')
 })
